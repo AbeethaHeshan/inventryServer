a23afb012b72ab57a470db55d2f0c8159bd55d77
Abeetha <abeethaheshan400@gmail.com>
2024-04-03 08:55:53 +0530

    init-project

---
+++ src/main/java/com/newinvent/inventryserver/InventryServerApplication.java
@@ -0,0 +1,13 @@
+package com.newinvent.inventryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newinvent/inventryserver/InventryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newinvent.inventryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
